b82d1a262052e165ebb425ec7743c6a532fda229
BlackBaroness <shoyurash2@gmail.com>
2021-05-22 01:28:00 +0300

    update checker added v12 release

--- src/main/java/ru/baronessdev/free/editor/BaronessEditor.java
+++ src/main/java/ru/baronessdev/free/editor/BaronessEditor.java
@@ -11,6 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.baronessdev.free.editor.handlers.CommandHandler;
 import ru.baronessdev.free.editor.handlers.ProjectHandler;
 import ru.baronessdev.free.editor.handlers.QueryHandler;
+import ru.baronessdev.free.editor.util.logging.LogType;
+import ru.baronessdev.free.editor.util.logging.Logger;
+import ru.baronessdev.free.editor.util.util.UpdateCheckerUtil;
 
 import java.io.File;
 
@@ -35,10 +38,11 @@ public final class BaronessEditor extends JavaPlugin {
         registerCompletions(manager);
 
         if (new File(getDataFolder() + File.separator + "projects").mkdirs()) {
-            System.out.println(ChatColor.AQUA + "[BaronessEditor] Projects dir created.");
+            Logger.log(LogType.INFO, "Projects dir created.");
         }
 
-        System.out.println(ChatColor.AQUA + "[BaronessEditor] Version " + getDescription().getVersion() + " enabled.");
+        Logger.log(LogType.INFO, "Version " + getDescription().getVersion() + " enabled.");
+        checkUpdates();
     }
 
     private void registerCommands(PaperCommandManager manager) {
@@ -55,5 +59,18 @@ public final class BaronessEditor extends JavaPlugin {
 
     public static String getMessage(String path) {
         return cfg.getString(path).replace('&', ChatColor.COLOR_CHAR);
+    }
+
+    private void checkUpdates() {
+        try {
+            int i = UpdateCheckerUtil.check(this);
+            if (i != -1) {
+                Logger.log(LogType.INFO, "New version found: v" + ChatColor.YELLOW + i + ChatColor.GRAY + " (Current: v" + getDescription().getVersion() + ")");
+                Logger.log(LogType.INFO, "Update now: " + ChatColor.AQUA + "market.baronessdev.ru/shop/licenses/");
+            }
+        } catch (UpdateCheckerUtil.UpdateCheckException e) {
+            Logger.log(LogType.ERROR, "Could not check for updates: " + e.getRootCause());
+            Logger.log(LogType.ERROR, "Please contact Baroness's Dev if this isn't your mistake.");
+        }
     }
 }
---
+++ src/main/java/ru/baronessdev/free/editor/util/logging/LogType.java
@@ -0,0 +1,7 @@
+package ru.baronessdev.free.editor.util.logging;
+
+public enum LogType {
+
+    INFO,
+    ERROR
+}
